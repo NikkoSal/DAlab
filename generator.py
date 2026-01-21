b4387c27ca789fdbd38ece1d785c6d16d831c6ff
Nikita Seliverstov <gamebot202@mail.ru>
2026-01-21 23:25:35 +1000

    data recording
    создал в mysql таблицу , куда в итоге перетекают все данные с генератора

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import time
 import random
 from datetime import datetime
+import mysql.connector
 
 cities = ["Москва", "Санкт-Петербург", "Владивосток", "Волгоград"]
 zones = ["Центр", "Спальный район", "Аэропорт", "Вокзал", "Бизнес-квартал"]
@@ -16,8 +17,8 @@ def generate_ride():
     ride = {
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "city": city,
-        "from": from_zone,
-        "to": to_zone,
+        "from_zone": from_zone,
+        "to_zone": to_zone,
         "distance_km": distance_km,
         "price": price,
         "rating": rating
@@ -25,9 +26,40 @@ def generate_ride():
 
     return ride
 
+def main():
+    connection = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='1111',
+        database='taxi'
+    )
+    cursor = connection.cursor()
+
+    try:
+        while True:
+            ride = generate_ride()
+            query = """
+                INSERT INTO rides (timestamp, city, from_zone, to_zone, distance_km, price, rating)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(query, (
+                ride["timestamp"],
+                ride["city"],
+                ride["from_zone"],
+                ride["to_zone"],
+                ride["distance_km"],
+                ride["price"],
+                ride["rating"]
+            ))
+            connection.commit()
+            print(ride)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        cursor.close()
+        connection.close()
 
 if __name__ == "__main__":
-    while True:
-        ride = generate_ride()
-        print(ride)
-        time.sleep(1)
+    main()
